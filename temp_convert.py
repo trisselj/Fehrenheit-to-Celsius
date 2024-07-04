cc53179eee15d974f1fc6ec72b1b13144556e4db
trisselj <trisselj@oregonstate.edu>
2024-07-03 18:40:20 -0700

    Spacing and date

--- temp_convert.py
+++ temp_convert.py
@@ -1,13 +1,16 @@
 # Author: Jake Trissel
 # Github Username: trisselj
-# Date: 06/26/2024
+# Date: 07/03/2024
 # Description: Converts from Celsius to Fahrenheit
 
 # Entering of temperature
 Celsius = input("Please enter the temperature in Celsius")
+
 # Str to float
 CelsiusFloat = float(Celsius)
+
 # Conversion
 FahrenheitFloat = (9/5) * CelsiusFloat + 32
+
 # Printing of answer
 print("The temperature you entered in Fahrenheit is " + str(FahrenheitFloat))
